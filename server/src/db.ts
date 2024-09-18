8c23642c30635f47e3b18410d8d6c535f247f357
Illia Piskurov <piskryov@gmail.com>
2024-09-18 12:38:08 +0300

    test db

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,9 +1,9 @@
 import mongoose, { ConnectOptions } from 'mongoose';
 
 mongoose.connect('mongodb://localhost:27017', {
-    user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
-    dbName: "ebooks"
+    user: "starlord",
+    pass: "0208",
+    dbName: "books"
 } as ConnectOptions).then(() => {
   console.log('Connected to MongoDB');
 }).catch((error) => {
