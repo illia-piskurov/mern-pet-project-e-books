bdfe287d91f68fcefbcebf5f3030de00ab14c3de
Illia Piskurov <piskryov@gmail.com>
2024-09-21 21:18:59 +0300

    add template for future navbar

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const HomePage: React.FC = () => {
   return (
     <Container>
       <Typography variant="h2">E-books</Typography>
+      <Navbar></Navbar>
       <Button variant="contained" color="primary" component={Link} to="/books">
         View Books
       </Button>
