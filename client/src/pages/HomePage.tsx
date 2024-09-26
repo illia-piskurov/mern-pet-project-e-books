3bdb38d6399c676fe2a4735f5d90ea2c27b2970b
Illia Piskurov <piskryov@gmail.com>
2024-09-26 14:28:03 +0300

    filtering works

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Container, Typography, Button, Grid } from '@mui/material';
 import Navbar from '../components/Navbar';
 import BookCard from '../components/BookCard';
 import { testBooks } from '../models/Books';
 
 const HomePage: React.FC = () => {
-  
-
   const [filter, setFilter] = useState('');
+  const filteredBooks = useMemo(() => {
+    return testBooks.filter(post => post.title.toLowerCase().includes(filter.toLowerCase()))
+  }, [filter, testBooks])
 
   return (
     <Container>
       <Typography variant="h2">E-books</Typography>
       <Navbar filter={filter} setFilter={setFilter}></Navbar>
       <Grid container spacing={2} sx={{ marginTop: 2 }}>
-        {testBooks.map((book, index) => (
+        {filteredBooks.map((book, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
-            <BookCard book={book} onEdit={() => {}} onDelete={() => {}} />
+            <BookCard book={book} onEdit={() => { }} onDelete={() => { }} />
           </Grid>
         ))}
       </Grid>
-      <Typography>{filter}</Typography>
     </Container>
   );
 };
