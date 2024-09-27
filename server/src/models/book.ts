8ef539f779ce3bb759092b78678e36bd00b904e2
Illia Piskurov <piskryov@gmail.com>
2024-09-27 12:32:09 +0300

    be changes

--- server/src/models/book.ts
+++ server/src/models/book.ts
@@ -4,21 +4,23 @@ import { Schema, model, Document } from 'mongoose';
 interface IBook extends Document {
     title: string;
     author: string;
-    description?: string;
-    publishDate?: Date;
-    pages?: number;
-    genre?: string;
-    createdAt?: Date;
+    description: string;
+    publishDate: string;
+    pages: number;
+    genre: string;
+    imageUrl: string;
+    downloadUrl: string;
 }
 
 const bookSchema = new Schema<IBook>({
     title: { type: String, required: true },
     author: { type: String, required: true },
-    description: { type: String },
-    publishDate: { type: Date },
+    description: { type: String, required: true },
+    publishDate: { type: String, required: true },
     pages: { type: Number },
-    genre: { type: String },
-    createdAt: { type: Date, default: Date.now }
+    genre: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    downloadUrl: { type: String, required: true }
 });
 
 const Book = model<IBook>('Book', bookSchema);
